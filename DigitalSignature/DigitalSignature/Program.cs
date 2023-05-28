777b86a2e428f677b56022f8f95a64b02120bea9
Edgar Jurkoit <90857989+edgaros24@users.noreply.github.com>
2023-05-28 14:26:25 +0300

    Add files via upload

---
+++ DigitalSignature/DigitalSignature/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Security.Cryptography;
+
+public class RSADigitalSignature
+{
+    static void Main()
+    {
+        // Generate RSA key pair
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            // Get the private and public keys
+            RSAParameters privateKey = rsa.ExportParameters(true);
+            RSAParameters publicKey = rsa.ExportParameters(false);
+
+            // Input text
+            Console.WriteLine("Enter text");
+            string message = Console.ReadLine();
+
+            // Compute the digital signature
+            byte[] signature = SignData(Encoding.UTF8.GetBytes(message), privateKey);
+
+            // Convert signature, message, and public key to base64 strings for transmission
+            string base64Signature = Convert.ToBase64String(signature);
+            string base64Message = Convert.ToBase64String(Encoding.UTF8.GetBytes(message));
+            string base64PublicKey = Convert.ToBase64String(Encoding.UTF8.GetBytes(GetPublicKeyString(publicKey)));
+
+            // Connect to the second application
+            using (var client = new TcpClient("127.0.0.1", 8080))
+            {
+                // Get the network stream for sending data
+                NetworkStream stream = client.GetStream();
+
+                // Send the message, signature, and public key
+                byte[] data = Encoding.UTF8.GetBytes(base64Message + "|" + base64Signature + "|" + base64PublicKey);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        Console.WriteLine("Message, digital signature, and public key sent. Press Enter to exit.");
+        Console.ReadLine();
+    }
+
+    // Method to compute the digital signature
+    static byte[] SignData(byte[] data, RSAParameters privateKey)
+    {
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.ImportParameters(privateKey);
+            return rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        }
+    }
+
+    // Method to get the string representation of the public key
+    static string GetPublicKeyString(RSAParameters publicKey)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(publicKey.Modulus.Length.ToString()).Append(":");
+        sb.Append(Convert.ToBase64String(publicKey.Modulus)).Append(":");
+        sb.Append(publicKey.Exponent.Length.ToString()).Append(":");
+        sb.Append(Convert.ToBase64String(publicKey.Exponent));
+        return sb.ToString();
+    }
+}
---
+++ DigitalValidation/DigitalValidation/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Security.Cryptography;
+
+public class RSADigitalSignatureValidation
+{
+    static void Main()
+    {
+        // Start listening on port 8080
+        TcpListener server = new TcpListener(IPAddress.Any, 8080);
+        server.Start();
+
+        Console.WriteLine("Waiting for connection...");
+
+        // Accept incoming connection
+        using (var client = server.AcceptTcpClient())
+        {
+            Console.WriteLine("Connected!");
+
+            // Get the network stream for receiving data
+            NetworkStream stream = client.GetStream();
+
+            // Receive the message, signature, and public key
+            byte[] data = new byte[4096];
+            int bytesRead = stream.Read(data, 0, data.Length);
+            string receivedData = Encoding.UTF8.GetString(data, 0, bytesRead);
+
+            // Split the received data into message, signature, and public key
+            string[] parts = receivedData.Split('|');
+            string base64Message = parts[0];
+            string base64Signature = parts[1];
+            string base64PublicKey = parts[2];
+
+            // Convert base64 strings back to byte arrays
+            byte[] messageBytes = Convert.FromBase64String(base64Message);
+            byte[] signature = Convert.FromBase64String(base64Signature);
+            byte[] publicKeyBytes = Convert.FromBase64String(base64PublicKey);
+
+            // Import the public key
+            RSAParameters publicKey = new RSAParameters
+            {
+                Modulus = publicKeyBytes,
+                Exponent = new byte[] { 1, 0, 1 } // Assuming a fixed exponent value of [1, 0, 1]
+            };
+
+            // Verify the digital signature
+            bool isSignatureValid = VerifyData(messageBytes, signature, publicKey);
+
+            if (isSignatureValid)
+            {
+                Console.WriteLine("Digital signature is valid.");
+            }
+            else
+            {
+                Console.WriteLine("Digital signature is not valid.");
+            }
+        }
+
+        // Stop listening
+        server.Stop();
+
+        Console.WriteLine("Press Enter to exit.");
+        Console.ReadLine();
+    }
+
+    // Method to verify the digital signature
+    static bool VerifyData(byte[] data, byte[] signature, RSAParameters publicKey)
+    {
+        using (var rsa = RSA.Create())
+        {
+            rsa.ImportParameters(publicKey);
+            return rsa.VerifyData(data, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        }
+    }
+}
